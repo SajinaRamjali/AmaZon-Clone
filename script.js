56c7ccc15128740dea28d8d1abecd7c9b0fe0506
SajinaRamjli <sajinaramjali1@gmail.com>
2025-06-03 22:26:13 -0700

    final project update

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const products = [
     { id: 1, name: "Wireless Headphone", price: 2000, image: "images/headphone.jpg" },
     { id: 2, name: "iPhone 15 Pro Max", price: 250000, image: "images/phone.jpg" },
-    { id: 3, name: "iWatch", price: 89000, image: "images/iWatch.jpg" },
-    { id: 4, name: "Speaker", price: 1000, image: "images/speaker.jpg" },
-    { id: 5, name: "Macbook Pro M4", price: 349000, image: "images/laptop.jpg" },
-    { id: 6, name: "Handmade Tulip Bouque", price: 3000, image: "images/Tulip.jpg" },
-    { id: 7, name: "Blue Rose Bouque", price: 3000, image: "images/bluerose.jpg" },
+    { id: 3, name: "Speaker", price: 1000, image: "images/speaker.jpg" },
+    { id: 4, name: "Macbook Pro M4", price: 349000, image: "images/laptop.jpg" },
+    { id: 5, name: "Handmade Tulip Bouque", price: 3000, image: "images/Tulip.jpg" },
+    { id: 6, name: "Blue Rose Bouque", price: 3000, image: "images/bluerose.jpg" },
 
 
 
